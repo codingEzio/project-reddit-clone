9fc172a8d4f5246c471427467614d2f09aed658e
!Mensch <assassinste@gmail.com>
2022-11-29 19:29:53 +0800

    feat: Send activation email asynchronously (faster response)

--- reddit-backend/src/main/java/com/elliot/reddit/service/MailService.java
+++ reddit-backend/src/main/java/com/elliot/reddit/service/MailService.java
@@ -8,6 +8,7 @@ import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import static com.elliot.reddit.util.Constants.EMAIL_SENDER_ADDR;
@@ -19,6 +20,7 @@ public class MailService {
 	private final JavaMailSender mailSender;
 	private final MailContentBuilder mailContentBuilder;
 
+	@Async
 	void sendMail(NotificationEmail notificationEmail) {
 		MimeMessagePreparator messagePreparator = mimeMessage -> {
 			MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
