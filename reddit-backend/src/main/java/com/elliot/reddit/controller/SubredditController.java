20215a437d3fb190a9f38dbb6d158532a1fa77c5
!Mensch <assassinste@gmail.com>
2022-11-30 10:21:16 +0800

    feat: Subreddit - fetch from DB and return

--- reddit-backend/src/main/java/com/elliot/reddit/controller/SubredditController.java
+++ reddit-backend/src/main/java/com/elliot/reddit/controller/SubredditController.java
@@ -3,18 +3,31 @@ package com.elliot.reddit.controller;
 import com.elliot.reddit.dto.SubredditDto;
 import com.elliot.reddit.service.SubredditService;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/subreddit")
 public class SubredditController {
 	private final SubredditService subredditService;
+
+	@GetMapping("/{id}")
+	public SubredditDto getSubreddit(@PathVariable Long id) {
+		return subredditService.getSubreddit(id);
+	}
+
+	@GetMapping
+	public List<SubredditDto> getAllSubreddits() {
+		return subredditService.getAllSubreddit();
+	}
 
 	@PostMapping
 	public SubredditDto create(@RequestBody @Valid SubredditDto subredditDto) {
--- reddit-backend/src/main/java/com/elliot/reddit/service/SubredditService.java
+++ reddit-backend/src/main/java/com/elliot/reddit/service/SubredditService.java
@@ -1,19 +1,44 @@
 package com.elliot.reddit.service;
 
 import com.elliot.reddit.dto.SubredditDto;
+import com.elliot.reddit.exception.SubredditNotFoundException;
 import com.elliot.reddit.model.Subreddit;
 import com.elliot.reddit.repository.SubredditRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 import static java.time.Instant.now;
+import static java.util.stream.Collectors.toList;
 
 @AllArgsConstructor
 @Service
 public class SubredditService {
 	private final SubredditRepository subredditRepository;
 	private final AuthService authService;
+
+	@Transactional(readOnly = true)
+	public SubredditDto getSubreddit(Long id) {
+		Subreddit subreddit = subredditRepository
+				.findById(id)
+				.orElseThrow(
+						() -> new SubredditNotFoundException(
+								"Subreddit not found with id - " + id
+						)
+				);
+
+		return mapToDto(subreddit);
+	}
+
+	@Transactional(readOnly = true)
+	public List<SubredditDto> getAllSubreddit() {
+		return subredditRepository.findAll()
+				.stream()
+				.map(this::mapToDto)
+				.collect(toList());
+	}
 
 	@Transactional
 	public SubredditDto save(SubredditDto subredditDto) {
@@ -22,6 +47,14 @@ public class SubredditService {
 		subredditDto.setId(subreddit.getId());
 
 		return subredditDto;
+	}
+
+	private SubredditDto mapToDto(Subreddit subreddit) {
+		return SubredditDto.builder()
+				.name(subreddit.getName())
+				.id(subreddit.getId())
+				.postCount(subreddit.getPosts().size())
+				.build();
 	}
 
 	private Subreddit mapToSubreddit(SubredditDto subredditDto) {
