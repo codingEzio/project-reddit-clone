7a5e2bf142cec39a43adde79405e7d9cc235450b
!Mensch <assassinste@gmail.com>
2022-12-01 18:02:55 +0800

    feat: Vote Post - attach relevant information to Post

--- reddit-backend/src/main/java/com/elliot/reddit/mapper/PostMapper.java
+++ reddit-backend/src/main/java/com/elliot/reddit/mapper/PostMapper.java
@@ -5,22 +5,36 @@ import com.elliot.reddit.dto.PostResponse;
 import com.elliot.reddit.model.Post;
 import com.elliot.reddit.model.Subreddit;
 import com.elliot.reddit.model.User;
+import com.elliot.reddit.repository.CommentRepository;
+import com.github.marlonlom.utilities.timeago.TimeAgo;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper(componentModel = "spring")
-public interface PostMapper {
+public abstract class PostMapper {
+	@Autowired
+	private CommentRepository commentRepository;
+
 	@Mapping(target = "createdDate", expression = "java(java.time.Instant.now())")
+	@Mapping(target = "description", source = "postRequest.description")
 	@Mapping(target = "subreddit", source = "subreddit")
 	@Mapping(target = "user", source = "user")
-	@Mapping(target = "description", source = "postRequest.description")
-	Post map(PostRequest postRequest, Subreddit subreddit, User user);
+	@Mapping(target = "voteCount", constant = "0")
+	public abstract Post map(PostRequest postRequest, Subreddit subreddit, User user);
 
 	@Mapping(target = "id", source = "postId")
-	@Mapping(target = "postName", source = "postName")
-	@Mapping(target = "description", source = "description")
-	@Mapping(target = "url", source = "url")
 	@Mapping(target = "subredditName", source = "subreddit.name")
 	@Mapping(target = "username", source = "user.username")
-	PostResponse mapToDto(Post post);
+	@Mapping(target = "commentCount", expression = "java(commentCount(post))")
+	@Mapping(target = "duration", expression = "java(getDuration(post))")
+	public abstract PostResponse mapToDto(Post post);
+
+	Integer commentCount(Post post) {
+		return commentRepository.findByPost(post).size();
+	}
+
+	String getDuration(Post post) {
+		return TimeAgo.using(post.getCreatedDate().toEpochMilli());
+	}
 }
