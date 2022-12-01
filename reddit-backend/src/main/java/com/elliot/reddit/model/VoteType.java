09279263cd49700a5eccd0f8cea9e8f5d2488f4a
!Mensch <assassinste@gmail.com>
2022-12-01 18:00:57 +0800

    dev: Utility method for enum VoteType

--- reddit-backend/src/main/java/com/elliot/reddit/model/VoteType.java
+++ reddit-backend/src/main/java/com/elliot/reddit/model/VoteType.java
@@ -1,10 +1,27 @@
 package com.elliot.reddit.model;
 
+import com.elliot.reddit.exception.SpringRedditException;
+
+import java.util.Arrays;
+
 public enum VoteType {
 	UPVOTE(1),
 	DOWNVOTE(-1);
 
-	VoteType (int direction) {
-		// pass
+	private int direction;
+
+	VoteType(int direction) {
+	}
+
+	public Integer getDirection() {
+		return direction;
+	}
+
+	public static VoteType lookup(Integer direction) {
+		return Arrays
+				.stream(VoteType.values())
+				.filter(value -> value.getDirection().equals(direction))
+				.findAny()
+				.orElseThrow(() -> new SpringRedditException("Vote not found"));
 	}
 }
