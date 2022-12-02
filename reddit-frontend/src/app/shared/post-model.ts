92029f08e4c724fa73431be58ae1304e61680aa0
!Mensch <assassinste@gmail.com>
2022-12-03 01:52:59 +0800

    dev: Common module to fetch the posts

---
+++ reddit-frontend/src/app/shared/post-model.ts
@@ -0,0 +1,10 @@
+export class PostModel {
+  id: number;
+  postName: string;
+  url: string;
+  description: string;
+  voteCount: number;
+  subredditName: string;
+  commentCount: number;
+  duration: string;
+}
