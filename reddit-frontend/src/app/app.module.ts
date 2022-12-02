fac0b781f9b1c4fec2adbccca15d5bc541a10eaa
!Mensch <assassinste@gmail.com>
2022-12-02 14:59:37 +0800

    refactor: Frontend signup - Integrate signup feature

--- reddit-frontend/src/app/app.module.ts
+++ reddit-frontend/src/app/app.module.ts
@@ -4,17 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { LoginComponent } from './auth/login/login.component';
-import { SignupComponent } from './auth/signup/signup.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    LoginComponent,
-    SignupComponent,
-  ],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, HeaderComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, AuthModule],
   providers: [],
   bootstrap: [AppComponent],
 })
