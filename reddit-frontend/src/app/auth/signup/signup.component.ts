ebfaefa0372035c6c417557bf928c7695315c041
!Mensch <assassinste@gmail.com>
2022-12-02 14:57:52 +0800

    feat: Frontend signup - Init/validate form data, POST request

--- reddit-frontend/src/app/auth/signup/signup.component.ts
+++ reddit-frontend/src/app/auth/signup/signup.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { SignupRequestPayload } from './signup-request.payload';
+import { AuthService } from '../shared/auth.service';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
+  signupForm: FormGroup;
+  signupRequestPayload: SignupRequestPayload;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private authService: AuthService) {
+    this.signupRequestPayload = {
+      email: '',
+      username: '',
+      password: '',
+    };
   }
 
+  ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+    });
+  }
+
+  signup() {
+    this.signupRequestPayload.email = this.signupForm.get('email').value;
+    this.signupRequestPayload.username = this.signupForm.get('username').value;
+    this.signupRequestPayload.password = this.signupForm.get('password').value;
+
+    this.authService.signup(this.signupRequestPayload).subscribe(
+      () => {
+        console.log('Signup successful');
+      },
+      () => {
+        console.log('Signup failed');
+      },
+    );
+  }
 }
