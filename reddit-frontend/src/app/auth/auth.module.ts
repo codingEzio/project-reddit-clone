4cd6226799ccaac998b9f7ae61c9bdb20a9f7c4a
!Mensch <assassinste@gmail.com>
2022-12-04 18:48:21 +0800

    chore: Generate frontend component for showing user profile

--- reddit-frontend/src/app/auth/auth.module.ts
+++ reddit-frontend/src/app/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { SignupComponent } from './signup/signup.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 @NgModule({
-  declarations: [SignupComponent, LoginComponent],
+  declarations: [SignupComponent, LoginComponent, UserProfileComponent],
   imports: [CommonModule, ReactiveFormsModule],
 })
 export class AuthModule {}
