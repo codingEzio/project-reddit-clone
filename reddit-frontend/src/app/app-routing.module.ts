0aaec6096ea23428715efc3cfd503209ad9191e0
!Mensch <assassinste@gmail.com>
2022-12-07 15:40:09 +0800

    dev: Setup routing for all non-auth paths

--- reddit-frontend/src/app/app-routing.module.ts
+++ reddit-frontend/src/app/app-routing.module.ts
@@ -1,19 +1,39 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { UserProfileComponent } from './auth/user-profile/user-profile.component';
 import { HomeComponent } from './home/home.component';
 import { CreatePostComponent } from './post/create-post/create-post.component';
+import { ViewPostComponent } from './post/view-post/view-post.component';
 import { CreateSubredditComponent } from './subreddit/create-subreddit/create-subreddit.component';
+import { ListSubredditsComponent } from './subreddit/list-subreddits/list-subreddits.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
 
+  { path: 'list-subreddits', component: ListSubredditsComponent },
+  { path: 'view-post/:id', component: ViewPostComponent },
+
   { path: 'signup', component: SignupComponent },
   { path: 'login', component: LoginComponent },
 
-  { path: 'create-subreddit', component: CreateSubredditComponent },
-  { path: 'create-post', component: CreatePostComponent },
+  {
+    path: 'user-profile/:name',
+    component: UserProfileComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'create-subreddit',
+    component: CreateSubredditComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'create-post',
+    component: CreatePostComponent,
+    canActivate: [AuthGuard],
+  },
 ];
 
 @NgModule({
