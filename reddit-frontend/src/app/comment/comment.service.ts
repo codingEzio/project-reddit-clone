99cfd2032bee15ad82752105f2cac07f825ebfc9
!Mensch <assassinste@gmail.com>
2022-12-07 13:36:01 +0800

    dev: Foundations for fetch and create comments

--- reddit-frontend/src/app/comment/comment.service.ts
+++ reddit-frontend/src/app/comment/comment.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { CommentPayload } from './comment-payload';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CommentService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getAllCommentsForPost(postId: number): Observable<CommentPayload[]> {
+    return this.http.get<CommentPayload[]>(
+      'http://localhost:8080/api/comments/by-postid/' + postId,
+    );
+  }
+
+  postComment(commentPayload: CommentPayload): Observable<any> {
+    return this.http.post<any>(
+      'http://localhost:8080/api/comments/',
+      commentPayload,
+    );
+  }
+
+  getAllCommentsByUser(name: string) {
+    return this.http.get<CommentPayload[]>(
+      'http://localhost:8080/api/comments/by-user/' + name,
+    );
+  }
 }
