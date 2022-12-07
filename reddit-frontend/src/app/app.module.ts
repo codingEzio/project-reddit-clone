7c5ed5150d4c13d4801d037bcfe527470e665fd0
!Mensch <assassinste@gmail.com>
2022-12-07 15:34:41 +0800

    refactor: Reorganized all the imports and declarations

--- reddit-frontend/src/app/app.module.ts
+++ reddit-frontend/src/app/app.module.ts
@@ -11,10 +11,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AuthModule } from './auth/auth.module';
 import { TokenInterceptor } from './token-interceptor';
 import { HomeComponent } from './home/home.component';
-import { PostTileComponent } from './shared/post-tile/post-tile.component';
 import { SideBarComponent } from './shared/side-bar/side-bar.component';
 import { SubredditSideBarComponent } from './shared/subreddit-side-bar/subreddit-side-bar.component';
 import { VoteButtonComponent } from './shared/vote-button/vote-button.component';
@@ -23,33 +21,39 @@ import { CreatePostComponent } from './post/create-post/create-post.component';
 import { ListSubredditsComponent } from './subreddit/list-subreddits/list-subreddits.component';
 import { EditorModule } from '@tinymce/tinymce-angular';
 import { ViewPostComponent } from './post/view-post/view-post.component';
+import { PostTileComponent } from './shared/post-tile/post-tile.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import { UserProfileComponent } from './auth/user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    SignupComponent,
+    LoginComponent,
     HomeComponent,
     PostTileComponent,
+    VoteButtonComponent,
     SideBarComponent,
     SubredditSideBarComponent,
-    VoteButtonComponent,
     CreateSubredditComponent,
     CreatePostComponent,
     ListSubredditsComponent,
     ViewPostComponent,
+    UserProfileComponent,
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule,
     AppRoutingModule,
+    ReactiveFormsModule,
     HttpClientModule,
-    AuthModule,
-    FontAwesomeModule,
-    NgbModule,
     NgxWebstorageModule.forRoot(),
     BrowserAnimationsModule,
     ToastrModule.forRoot(),
+    FontAwesomeModule,
     EditorModule,
+    NgbModule,
   ],
   providers: [
     {
