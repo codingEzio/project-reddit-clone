fd781bdcf2e44380548cf4306b0fe6cdad0be924
!Mensch <assassinste@gmail.com>
2022-12-07 13:34:34 +0800

    dev: Fetch and create posts

--- reddit-frontend/src/app/shared/post-model.ts
+++ reddit-frontend/src/app/shared/post-model.ts
@@ -4,7 +4,10 @@ export class PostModel {
   url: string;
   description: string;
   voteCount: number;
+  username: string;
   subredditName: string;
   commentCount: number;
   duration: string;
+  upVote: boolean;
+  downVote: boolean;
 }
