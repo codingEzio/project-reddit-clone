cb6c438173867efea768112edb7d1b6b94ba61dd
!Mensch <assassinste@gmail.com>
2022-12-07 13:33:24 +0800

    feat: Foundations for the look and feat of Vote

--- reddit-frontend/src/app/shared/vote-button/vote-button.component.ts
+++ reddit-frontend/src/app/shared/vote-button/vote-button.component.ts
@@ -1,15 +1,75 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { faArrowUp, faArrowDown } from '@fortawesome/free-solid-svg-icons';
+import { ToastrService } from 'ngx-toastr';
+
+import { PostModel } from '../post-model';
+import { AuthService } from 'src/app/auth/shared/auth.service';
+import { PostService } from '../post.service';
+import { VoteService } from '../vote.service';
+import { VotePayload } from './vote-payload';
+import { throwError } from 'rxjs';
+import { VoteType } from './vote-type';
 
 @Component({
   selector: 'app-vote-button',
   templateUrl: './vote-button.component.html',
-  styleUrls: ['./vote-button.component.css']
+  styleUrls: ['./vote-button.component.css'],
 })
 export class VoteButtonComponent implements OnInit {
+  @Input() post: PostModel;
+  votePayload: VotePayload;
+  faArrowUp = faArrowUp;
+  faArrowDown = faArrowDown;
+  upvoteColor: string;
+  downvoteColor: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private voteService: VoteService,
+    private authService: AuthService,
+    private postService: PostService,
+    private toastr: ToastrService,
+  ) {
+    this.votePayload = {
+      voteType: undefined,
+      postId: undefined,
+    };
   }
 
+  ngOnInit(): void {
+    this.updateVoteDetails();
+  }
+
+  upvotePost() {
+    this.votePayload.voteType = VoteType.UPVOTE;
+
+    this.vote();
+    this.downvoteColor = '';
+  }
+
+  downvotePost() {
+    this.votePayload.voteType = VoteType.DOWNVOTE;
+
+    this.vote();
+    this.upvoteColor = '';
+  }
+
+  private vote() {
+    this.votePayload.postId = this.post.id;
+
+    this.voteService.vote(this.votePayload).subscribe(
+      () => {
+        this.updateVoteDetails();
+      },
+      error => {
+        this.toastr.error(error.error.message);
+        throwError(error);
+      },
+    );
+  }
+
+  private updateVoteDetails() {
+    this.postService.getPost(this.post.id).subscribe(post => {
+      this.post = post;
+    });
+  }
 }
